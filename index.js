a34e141c197eb659291f69d8a32a7f683e769992
Charles Mangwa <charlesmangwa@gmail.com>
2017-05-20 11:57:05 +0200

    🎉  Final commit 🎉

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { cacheAssets, registerServiceWorker } from "./assets/serviceWorkerHelpers";
 import Presentation from "./presentation";
 
+if ("serviceWorker" in navigator && process.env.NODE_ENV === "production") {
+  registerServiceWorker();
+
+  const assets = [ // list of urls to be cached
+    "/",
+    "/200.html",
+    "/index.html",
+    "/dist/bundle.js",
+    "/dist/04d63387484bf5e2e47bf5ef066f1fcb.gif",
+    "/dist/04d63387484bf5e2e47bf5ef066f1fcb.gif",
+    "/dist/56aa12171a181c06a24264d64a975efb.png",
+    "/dist/069fb145391042f9926fc4549f23518f.jpg",
+    "/dist/3835d803d3ecc1301d0bf5636cb38bf5.gif",
+    "/dist/9860ae4b5377caea075e38e03bfc0852.gif"
+  ];
+
+  // // cache responses of provided urls
+  cacheAssets(assets).then(() => {
+    console.log("Assets successfully cached 🙌");
+  });
+}
 render(<Presentation/>, document.getElementById("root"));
--- presentation/index.js
+++ presentation/index.js
@@ -67,6 +67,20 @@ export default class Presentation extends React.Component {
           transition={["slide"]}
           bgImage={images.background}
           bgDarken={0.5}
+          notes={ 
+            <ul>
+              <li>Hi eveyrone...</li>
+              <li>Aka The Talk Where...</li>
+              <li>Let me start by introducing myself real quick:
+                  <ul>
+                      <li>I’m Charles</li>
+                      <li>Student here in Paris</li>
+                      <li> I’ve been a volunteer</li>
+                      <li> And I've discovered React Native back in June 2015</li>
+                  </ul>
+              </li>
+          </ul>
+        }
         >
           <Heading size={4} caps lineHeight={1} textColor="primary">
             ⛵️ Thousand Ways to Navigate in React Native
@@ -107,7 +121,7 @@ export default class Presentation extends React.Component {
           </Heading>
           <Appear>
             <BlockQuote margin="50px 0 0">
-              <Quote>The smallest detail can f**k up the whole experience bro!</Quote>
+              <Quote>The smallest detail can f**k up the whole user experience!</Quote>
               <Cite>Léo Le Bras</Cite>
             </BlockQuote>
           </Appear>
@@ -135,6 +149,13 @@ export default class Presentation extends React.Component {
           textColor="primary"
           bgImage={images.background}
           bgDarken={0.8}
+          notes={
+            <ul>
+              <li>Don’t worry: this is a drop in the ocean of what you can find on NPM</li>
+              <li><i>DRINK! </i>🥛</li>
+              <li>That’s the part of the talk where you should start understanding its title</li>
+            </ul>
+          }
         >
           <Image src={images.libraries} width={885} height={590} />
           <Heading size={4} margin="30px 0 0" textColor="minions">
@@ -197,16 +218,80 @@ export default class Presentation extends React.Component {
             </BlockQuote>
           </Appear>
         </Slide>
+        <Slide
+          transition={["slide"]}
+          textColor="primary"
+          bgImage={images.background}
+          bgDarken={0.8}
+          notes={
+            <ul>
+              <li><i>DRINK! </i>🥛</li>
+              <li>Yeah, I think some of you might see where I’m going with this 😏...</li>
+            </ul>
+          }
+        >
+          <Heading size={6} textColor="tertiary" caps>
+            react-router-native
+          </Heading>
+             <BlockQuote margin="50px 0 0">
+             <Quote>
+                <Heading size={3} textColor="primary" textAlign="center center">It doesn't ship with a navigation structure</Heading>
+              </Quote>
+            </BlockQuote>
+        </Slide>
+        <Slide transition={["slide"]} bgImage={images.background} bgDarken={0.8}>
+          <Heading size={4} textColor="primary" margin="0 0 25px">
+            🎉 react-native-router-redux-flux-ex-navigation-experimental-awesome-web-primitives 🎉
+          </Heading>
+          <Code textColor="primary">v16.0.0-alpha.42</Code>
+        </Slide>
         <Slide transition={["slide"]} bgImage={images.cheering} bgDarken={0.3}>
           <Heading size={1} textColor="primary" margin="0 0 25px" fit>
             🎉 react-router-navigation 🎉
           </Heading>
-          <Code textColor="primary">v1</Code>
+          <Code textColor="primary">github.com/LeoLeBras/react-router-navigation</Code>
         </Slide>
         <Slide
           transition={["slide"]}
           bgImage={images.background}
           bgDarken={0.8}
+        >
+          <Heading size={6} textColor="tertiary" caps>
+            The idea
+          </Heading>
+          <CodePane
+            lang="js"
+            textSize={25}
+            margin="25px 0 0"
+            source={require("raw-loader!../assets/reactrouter.example")}
+          />
+        </Slide>
+        <Slide
+          transition={["slide"]}
+          bgImage={images.background}
+          bgDarken={0.8}
+        >
+          <Heading size={6} textColor="tertiary" caps>
+            The idea
+          </Heading>
+          <CodePane
+            lang="js"
+            textSize={25}
+            margin="25px 0 0"
+            source={require("raw-loader!../assets/reactrouternavigation.example")}
+          />
+        </Slide>
+        <Slide
+          transition={["slide"]}
+          bgImage={images.background}
+          bgDarken={0.8}
+          notes={
+            <ul>
+              <li><code class='code-inline'>{`<Navigation />`}</code> which renders
+                  one screen at a time and provides transitions between screens
+              </li>
+            </ul>
+          }
         >
           <Heading size={6} textColor="primary">Navigation</Heading>
           <CodePane
@@ -220,6 +305,13 @@ export default class Presentation extends React.Component {
           transition={["slide"]}
           bgImage={images.background}
           bgDarken={0.8}
+          notes={
+            <ul>
+              <li>These screens which are in turn wrapped into our 2nd component: <code class='code-inline'>{`<Card />`}</code></li>
+              <li>So, what you see here is the simplest way to get up and running with <code class='code-inline'>{`react-router-navigation`}</code></li>
+              <li>As you can see: we tried to keep the same API</li>
+            </ul>
+          }
         >
           <Heading size={6} textColor="primary">Card</Heading>
           <CodePane
@@ -233,6 +325,12 @@ export default class Presentation extends React.Component {
           transition={["slide"]}
           bgImage={images.background}
           bgDarken={0.8}
+          notes={
+            <ul>
+              <li>The next 2 components are quite similar</li>
+              <li>If you’ve ever struggled navigating inside tab views</li>
+            </ul>
+          }
         >
           <Heading size={6} textColor="primary">Tabs & Tab</Heading>
           <CodePane
@@ -246,6 +344,12 @@ export default class Presentation extends React.Component {
           transition={["slide"]}
           bgImage={images.background}
           bgDarken={0.8}
+          notes={
+            <ul>
+              <li>And last but not least, the core element of our navigation</li>
+              <li>As you can see: except particular cases, we tried to keep a constant API among all the components</li>
+            </ul>
+          }
         >
           <Heading size={6} textColor="primary">Bottom navigation</Heading>
           <CodePane
@@ -276,8 +380,16 @@ export default class Presentation extends React.Component {
           textColor="tertiary"
           bgImage={images.background}
           bgDarken={0.8}
+          notes={
+            <ul>
+              <li><strong><i>The real heroes</i></strong>, • give props • great people • libraries we rely on</li>
+              <li>I'm talking about...</li>
+              <li>We use for navigation, transitions, routing, tab bars, deep linking, and a bunch of other cool stuff</li>
+              <li>So: let's give a round of applause please, to everyone working on these libraries!</li>
+            </ul>
+          }
         >
-          <Heading size={6} textColor="tertiary" caps>Shoutouts</Heading>
+          <Heading size={6} textColor="tertiary" caps>The real heroes</Heading>
           <Appear>
             <List>
               <ListItem>
@@ -301,7 +413,7 @@ export default class Presentation extends React.Component {
           bgImage={images.background}
           bgDarken={0.8}
         >
-          <Heading size={6} textColor="tertiary" caps>Any questions?</Heading>
+          <Heading size={6} textColor="tertiary" caps>We'd love to hear from you!</Heading>
           <Appear>
             <Text margin="30px 0 0" textSize={33} textColor="primary" fit>
               👉 Medium article: Thousand Ways to Navigate in React Native 👈
@@ -309,7 +421,12 @@ export default class Presentation extends React.Component {
           </Appear>
           <Appear>
             <Text margin="50px 0 30px" textSize={35} textColor="primary">
-              🐥 @Leo_LeBras • @Charles_Mangwa 🐥
+              @Leo_LeBras • @Charles_Mangwa
+            </Text>
+          </Appear>
+          <Appear>
+            <Text margin="50px 0 30px" textSize={30} textColor="primary">
+              Coming next: native navigation 👀
             </Text>
           </Appear>
           <Appear>
